cc98dfcf270a5547dd9046096739e6a173b2ecb8
Babalola Muhammad <barmilitary@yahoo.com>
2022-05-27 13:31:41 +0100

    folder structure done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { registerLicense } from '@syncfusion/ej2-base';
+
 import App from "./App";
+import './index.css';
+
+registerLicense('ORg4AjUWIQA/Gnt2VVhhQlFac1pJW3xLeEx0RWFbb1l6dlxMYFlBNQtUQF1hS35WdEJjXntddHNWQGdf');
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
